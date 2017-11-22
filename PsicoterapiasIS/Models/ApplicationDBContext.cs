e97f1fbf7ff266ecdedfde7e83894b525d7d2f02
Arturo Nevarez <arturo-08@hotmail.com>
2017-11-22 14:11:26 -0800

    Clase DbContext

---
+++ PsicoterapiasIS/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PsicoterapiasIS.Models
+{
+    public class ApplicationDBContext: DbContext 
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
---
+++ PsicoterapiasIS/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PsicoterapiasIS.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNac { get; set; }
+
+
+    }
+}
